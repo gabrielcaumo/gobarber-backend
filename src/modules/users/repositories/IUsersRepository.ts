1bb8e29d35d37e2b6ace56e077be4ce06ffa5a21
gabrielcaumo <gabriel_caumo@yahoo.com.br>
2020-05-10 16:15:13 -0300

    Provider list implementation

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProivdersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
+  findAllProviders(data: IFindAllProivdersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
